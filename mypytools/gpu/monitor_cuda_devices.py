28f342897a99f6f7c2009501dcd9c201d1181a44
Zekun Lou <zekun.lou@mpsd.mpg.de>
2024-06-25 18:09:45 +0200

    update monitoring

--- mypytools/gpu/monitor_cuda_devices.py
+++ mypytools/gpu/monitor_cuda_devices.py
@@ -4,26 +4,24 @@ import json
 import time
 
 
-def monitor_gpus(interval: int = 10, duration: int = 24 * 3600, log_file="gpu_usage.log"):
+def monitor_gpus(interval: int = 10, duration: int = 24 * 3600, log_fpath="gpu_usage.log"):
     from nvitop import Device
 
     start_time = time.time()
-    with open(log_file, "w") as f:
+    with open(log_fpath, "w") as f:
         while (time.time() - start_time) < duration:
             timestamp = datetime.datetime.now().isoformat()
             devices = Device.all()
-            stats = {
-                "timestamp": timestamp,
-                "devices": dict(),
-            }
             for device in devices:
-                stats["devices"][device.index] = {
+                stats = {
+                    "timestamp": timestamp,
+                    "gpu_id": device.index,
                     "temperature": device.temperature(),
                     "gpu_utilization": device.gpu_utilization(),
                     "memory_total": device.memory_total(),
                     "memory_used": device.memory_used(),
                 }
-            f.write(json.dumps(stats) + "\n")
+                f.write(json.dumps(stats) + "\n")
             f.flush()
             time.sleep(interval)
 
@@ -68,12 +66,13 @@ def visualize_gpus_usage(log_fpath: str, ax=None):
         ax[0].plot(grp.index, grp["temperature"], label=f"GPU {key} Temperature", linestyle="--")
 
     ax[0].set_title("GPU Utilization (%) and Temperature (C)")
-    ax[0].set_ylabel("Percentage / Celsius")
+    ax[0].set_ylabel("Percentage / Degree")
     ax[0].legend()
 
     # Plot Memory Usage
-    for key, grp in df.groupby("gpu_id"):
-        ax[1].plot(grp.index, grp["memory_used"], label=f"GPU {key} Memory Used")
+    for color_idx, (key, grp) in enumerate(df.groupby("gpu_id")):
+        ax[1].plot(grp.index, grp["memory_used"], color=f"C{color_idx}", label=f"GPU {key}")
+        ax[1].axhline(grp["memory_total"][0], min(grp.index), max(grp.index), linestyle="dotted", color=f"C{color_idx}")
 
     ax[1].set_title("Memory Used (MiB)")
     ax[1].set_ylabel("Memory (MiB)")
@@ -89,7 +88,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Monitor NVIDIA GPU status")
     parser.add_argument("--interval", "-i", type=int, default=10, help="Recording time interval in seconds")
     parser.add_argument("--duration", "-d", type=int, default=24 * 3600, help="Maximum monitoring duration in seconds")
-    parser.add_argument("--log_file", "-l", type=str, default="gpu_usage.log", help="Logging file path")
+    parser.add_argument("--log_fpath", "-l", type=str, default="gpu_usage.log", help="Logging file path")
     args = parser.parse_args()
 
     monitor_gpus(**vars(args))
