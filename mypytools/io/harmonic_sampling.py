c7f713c3cd98eb3968d8b9887c3760e984524cf3
Zekun Lou <zekun.lou@mpsd.mpg.de>
2025-02-25 22:04:51 +0100

    update harmonic sampling methods

--- mypytools/io/harmonic_sampling.py
+++ mypytools/io/harmonic_sampling.py
@@ -12,12 +12,7 @@ UNIT_PH_AMP = 0.28725069  # Angstrom, = \sqrt{\hbar^2} / \sqrt{2 * 1u * 1eV}
 KELVIN_TO_EV = 8.61733e-5  # so 100K = 8.6meV
 
 
-"""
-
-"""
-
-
-def gen_amp_by_modes(
+def gen_amp_by_modes_wrong(
     normal_modes: numpy.ndarray,  # shape (natoms*3, nmodes), normalized to 1
     mode_energies: numpy.ndarray,  # shape (nmodes,), in eV
     amp_factor: float = 1.0,
@@ -29,6 +24,9 @@ def gen_amp_by_modes(
     # seed: int = 0,
     verbose: bool = False,
 ):
+    """
+    this is written on 250215, mistaken (n+1/2)^{0.5} as (n+1/2)
+    """
     r"""
     Equation: see `notes_research`, file `physics/EPC/phonon BvK conditions`, section `Harmonic sampling`
 
@@ -37,6 +35,7 @@ def gen_amp_by_modes(
         with $M_0 = 1u$, remember to multiply by $1 / \sqrt{M_\kappa}$ for the final amplitude
         but for `random` cases, just a scaled random number
     """
+    print("please consider switching to the correct function")
     if verbose:
         log = print
     else:
@@ -84,6 +83,78 @@ def gen_amp_by_modes(
     return amp_by_modes, mode_energies, normal_modes
 
 
+
+def gen_amp_by_modes(
+    normal_modes: numpy.ndarray,  # shape (natoms*3, nmodes), normalized to 1
+    mode_energies: numpy.ndarray,  # shape (nmodes,), in eV
+    amp_factor: float = 1.0,
+    mode_energy_cutoff: Optional[float] = None,  # in eV
+    amp_method: Literal["maxwell", "bosonic", "highT", "uniform", "random", "manual"] = "manual",
+    temperature: Optional[float] = None,  # if bosonic or uniform, in Kelvin
+    amp_by_modes: Optional[numpy.ndarray] = None,  # if manual
+    with_ZPM: Optional[bool] = None,
+    # seed: int = 0,
+    verbose: bool = False,
+):
+    r"""
+    Equation: see `notes_research`, file `physics/EPC/phonon BvK conditions`, section `Harmonic sampling`
+
+    Return: numpy.ndarray, shape (nmodes,)
+        amplitude, as defined by $$ A f(\omega) \sqrt{\frac{\hbar^2}{2 N_p M_0 \hbar \omega_\nu}} $$
+        with $M_0 = 1u$, remember to multiply by $1 / \sqrt{M_\kappa}$ for the final amplitude
+        but for `random` cases, just a scaled random number
+    """
+    if verbose:
+        log = print
+    else:
+        log = lambda *args, **kwargs: None  # NOQA
+    # numpy.random.seed(seed)
+    nmodes = mode_energies.shape[0]
+    if amp_method in ("maxwell", "bosonic"):
+        assert temperature is not None, "temperature should be provided for maxwell method"
+        assert mode_energy_cutoff is not None, "mode_energy_cutoff should be provided for maxwell method"
+        assert (with_ZPM is not None) and isinstance(with_ZPM, bool), \
+            "with_ZPM should be provided for maxwell method"
+        # apply energy cutoff
+        log(f"energy cutoff: {mode_energy_cutoff=:.3e} eV")
+        modes_keep_indices = numpy.where(mode_energies > mode_energy_cutoff)[0]
+        assert len(modes_keep_indices) > 0, "no modes left after energy cutoff, decrease mode_energy_cutoff"
+        log(f"energy cutoff: total {nmodes} modes, remaining {len(modes_keep_indices)} modes")
+        mode_energies = mode_energies[modes_keep_indices]
+        normal_modes = normal_modes[:, modes_keep_indices]
+        # calculate the mode amplitudes
+        exp_e_by_t = numpy.exp(mode_energies / (KELVIN_TO_EV * temperature))
+        if "maxwell" in amp_method:
+            amp_by_modes = (1 / exp_e_by_t + (0.5 if with_ZPM else 0))**0.5
+        elif "bosonic" in amp_method:
+            amp_by_modes = (1 / (exp_e_by_t - 1) + (0.5 if with_ZPM else 0))**0.5
+        amp_by_modes *= amp_factor * UNIT_PH_AMP / numpy.sqrt(mode_energies)  # shape (nmodes,)
+    elif amp_method in ("highT", "uniform"):
+        assert mode_energy_cutoff is not None, "mode_energy_cutoff should be provided for uniform method"
+        modes_keep_indices = numpy.where(mode_energies > mode_energy_cutoff)[0]
+        assert len(modes_keep_indices) > 0, "no modes left after energy cutoff, decrease mode_energy_cutoff"
+        log(f"energy cutoff: total {nmodes} modes, remaining {len(modes_keep_indices)} modes")
+        mode_energies = mode_energies[modes_keep_indices]
+        normal_modes = normal_modes[:, modes_keep_indices]
+        if amp_method == "uniform":
+            amp_by_modes = amp_factor * UNIT_PH_AMP / numpy.sqrt(mode_energies)
+        elif "highT" in amp_method:
+            assert temperature is not None, "temperature should be provided for highT method"
+            amp_by_modes = amp_factor * UNIT_PH_AMP * numpy.sqrt(KELVIN_TO_EV * temperature) / mode_energies
+    elif amp_method == "random":
+        # assume mode as 1eV, just to ensure a similar magnitude
+        amp_by_modes = amp_factor * UNIT_PH_AMP
+    elif amp_method == "manual":
+        assert amp_by_modes is not None, "amp_by_band should be provided for manual method"
+        assert (
+            len(amp_by_modes) == mode_energies.shape[0]
+        ), f"inconsistent shapes: {len(amp_by_modes)=}, {mode_energies.shape=}"
+        amp_by_modes = amp_by_modes  # shape (nmodes,)
+    else:
+        raise ValueError(f"unsupported amp_method: {amp_method}")
+    return amp_by_modes, mode_energies, normal_modes
+
+
 def harmonic_sampling(
     atoms_ref: Atoms,  # length unit: Angstrom
     normal_modes: numpy.ndarray,  # shape (natoms*xyz, nmodes), normalized to 1
@@ -91,7 +162,7 @@ def harmonic_sampling(
     n_frames: int,
     amp_factor: float = 1.0,
     mode_energy_cutoff: Optional[float] = None,  # in eV
-    amp_method: Literal["maxwell", "bosonic", "uniform", "random", "manual"] = "manual",
+    amp_method: Literal["maxwell", "bosonic", "highT", "uniform", "random", "manual"] = "manual",
     temperature: Optional[float] = None,  # if bosonic or uniform, in Kelvin
     amp_by_modes: Optional[numpy.ndarray] = None,  # if manual
     with_ZPM: Optional[bool] = None,
@@ -103,10 +174,11 @@ def harmonic_sampling(
     Equation: see `notes_research`, file `physics/EPC/phonon BvK conditions`, section `Harmonic sampling`
 
     Parameter `amp_method`: amplitude generation methods
-        - maxwell: generate amplitudes according to Maxwell-Boltzmann distribution
-        - bosonic: generate amplitudes according to Bose-Einstein distribution (with ZPM)
-        - uniform: generate amplitudes randomly but with amp factor $1 / \sqrt{\omega}$
-        - random: generate amplitudes randomly without considering the ZPM amplitude
+        - maxwell: according to Maxwell-Boltzmann distribution
+        - bosonic: according to Bose-Einstein distribution (with ZPM)
+        - highT: according to Bose-Einstein distribution but assume high temperature (so no ZPM)
+        - uniform: randomly but with amp factor $1 / \sqrt{\omega}$
+        - random: randomly without considering the ZPM amplitude
         - manual: use the provided amplitudes, which should be a 1D array of length nmodes
     """
     if verbose:
