ed821d10ad7e1c9521bdeb65bda87f0475bdaf1c
Zekun Lou <zekun.lou@mpsd.mpg.de>
2025-07-24 11:19:01 +0200

    minor update

--- mypytools/aims/parse_out.py
+++ mypytools/aims/parse_out.py
@@ -34,9 +34,9 @@ _regexp_dict = {  # energy in eV
     "number_of_atoms": re.compile(r"\|\s*Number of atoms\s*:\s*(\d+)"),
     "electrostatic_energy": re.compile(r"\|\s*Electrostatic energy\s*:\s*-?\d+\.\d+\s*Ha\s*(-?\d+\.\d+)\s*eV"),
     "xc_energy": re.compile(r"\|\s*XC energy correction\s*:\s*-?\d+\.\d+\s*Ha\s*(-?\d+\.\d+)\s*eV"),
-    "total_energy_per_atom": re.compile(
-        r"\|\s*Electronic free energy per atom\s*:\s*-?\d+\.\d+\s*Ha\s*(-?\d+\.\d+)\s*eV"
-    ),
+    "total_energy_per_atom": re.compile(r"\|\s*Electronic free energy per atom\s*:\s*(-?\d+\.\d+)\s*eV"),
+    # "total_energy": re.compile(r"\|\s*Electronic free energy\s*:\s*-?\d+\.\d+\s*Ha\s*(-?\d+\.\d+)\s*eV"),
+    # # it is correct actually, because the total energy per atom doesn't have Hartree value so re matching is correct.
 }
 
 
